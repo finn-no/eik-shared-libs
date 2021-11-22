f03c05a43fe80c400de88a3707e52c82dec357a0
Richard Walker <richard.walker@schibsted.com>
2021-11-22 17:18:07 +0100

    build: add publishing to eik

---
+++ scripts/publish.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+import { readFileSync } from 'fs';
+import { join } from 'path';
+import eik from '@eik/cli';
+
+// usage: EIK_TOKEN=<eik token> ./publish.js <name of package folder> <name of dependency>
+// example: EIK_TOKEN=<eik token> ./publish.js react @esm-bundle/react
+
+const { EIK_TOKEN } = process.env;
+const dirname = new URL('./', import.meta.url).pathname;
+const [, , packageNameArg, dependencyNameArg] = process.argv;
+const cwd = join(dirname, '../packages', packageNameArg);
+
+// read package.json file
+const packageJSONPath = join(cwd, './package.json');
+const packageJSON = JSON.parse(readFileSync(packageJSONPath, 'utf8'));
+
+// grab Eik variables from package.json
+const { eik: { server, files, type }, name } = packageJSON;
+
+// grab version directly from the dependency
+const version = packageJSON.dependencies[dependencyNameArg];
+
+const token = await eik.login({
+    server,
+    key: EIK_TOKEN,
+    logger: console,
+});
+
+await eik.publish({
+    cwd,
+    token,
+    server,
+    name,
+    type,
+    version,
+    files,
+    logger: console,
+});
