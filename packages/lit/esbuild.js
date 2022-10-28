6ac7e318d1b075b8e5311039c06c41a3a8a4abb6
Richard Walker <digitalsadhu@gmail.com>
2022-10-28 14:16:30 +1300

    fix: replace rollup with esbuild to avoid peer dep hell

---
+++ packages/lit/esbuild.js
@@ -0,0 +1,11 @@
+import esbuild from 'esbuild';
+
+await esbuild.build({
+    entryPoints: ['./index.js'],
+    bundle: true,
+    minify: true,
+    format: 'esm',
+    outfile: 'dist/lit.min.js',
+    target: ['es2017'],
+    sourcemap: true,
+});
