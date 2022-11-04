e2fb9203081753e95f9be1613f16be2e5512b7ce
Richard Walker <digitalsadhu@gmail.com>
2022-11-04 14:28:54 +1300

    fix: fixup publish scripts

--- scripts/publish.js
+++ scripts/publish.js
@@ -3,13 +3,14 @@
 import { readFileSync } from 'fs';
 import { join } from 'path';
 import eik from '@eik/cli';
+import semver from 'semver';
 
 // usage: EIK_TOKEN=<eik token> ./publish.js <name of package folder> <name of dependency>
 // example: EIK_TOKEN=<eik token> ./publish.js react @esm-bundle/react
 
 const { EIK_TOKEN } = process.env;
 const dirname = new URL('./', import.meta.url).pathname;
-const [, , packageNameArg, dependencyNameArg] = process.argv;
+const [, , packageNameArg, dependencyNameArg, stripPrerelease] = process.argv;
 const cwd = join(dirname, '../packages', packageNameArg);
 
 // read package.json file
@@ -17,10 +18,17 @@ const packageJSONPath = join(cwd, './package.json');
 const packageJSON = JSON.parse(readFileSync(packageJSONPath, 'utf8'));
 
 // grab Eik variables from package.json
-const { eik: { server, files, type }, name } = packageJSON;
+const { eik: { name: eikName, server, files, type }, name: pkgName } = packageJSON;
 
 // grab version directly from the dependency
-const version = packageJSON.dependencies[dependencyNameArg];
+let version = packageJSON.dependencies[dependencyNameArg];
+
+if (stripPrerelease) {
+    version = `${semver.major(version)}.${semver.minor(version)}.${semver.patch(version)}`;
+}
+
+// allow eik name field to overwrite package name field
+const name = eikName || pkgName;
 
 const token = await eik.login({
     server,
