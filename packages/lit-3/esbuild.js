b6868aea197f4c7f10663a91c0e19a3ade3c922a
Richard Walker <digitalsadhu@gmail.com>
2024-01-02 14:59:52 +1300

    fix: improve lit static bundle size

--- packages/lit-3/esbuild.js
+++ packages/lit-3/esbuild.js
@@ -1,4 +1,7 @@
 import esbuild from "esbuild";
+import { plugin, load } from "@eik/esbuild-plugin";
+
+await load({ maps: [{ imports: { "./lit-html.js": "./lit.min.js" } }] });
 
 await esbuild.build({
   entryPoints: ["./lit.min.js", "static-html.js"],
@@ -8,4 +11,5 @@ await esbuild.build({
   outdir: "dist/",
   target: ["es2017"],
   sourcemap: true,
+  plugins: [plugin()],
 });
