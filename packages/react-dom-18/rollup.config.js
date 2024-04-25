dd8781fd297358e87e50714723769c3f97ce85a5
William Killerud <william.killerud@schibsted.com>
2024-04-25 13:11:35 +0200

    chore: drop build of version-specific files
    There's already a versioned URL, and these versioned files are not
    on Eik now. If folks use alias + versioned URL it equals bad time.

--- packages/react-dom-18/rollup.config.js
+++ packages/react-dom-18/rollup.config.js
@@ -22,7 +22,7 @@ const {
 const browserslistrc = new URL("../../.browserslistrc", import.meta.url);
 const browserslist = fs.readFileSync(browserslistrc.pathname, "utf-8");
 
-const pluginsAlias = [
+const plugins = [
   eikPlugin({
     maps: [
       {
@@ -47,73 +47,25 @@ const pluginsAlias = [
   env === "production" ? terser({ format: { comments: false } }) : null,
 ];
 
-const pluginsSpecificVersion = [
-  eikPlugin({
-    maps: [
-      {
-        imports: {
-          react: `https://assets.finn.no/npm/${name}/${version}/react.${env}.min.js`,
-        },
-      },
-    ],
-  }),
-  nodeResolve(),
-  commonjs({ include: /node_modules/ }),
-  replace({ preventAssignment: true, values: { "process.env.NODE_ENV": JSON.stringify(env) } }),
-  env === "production" ? terser({ format: { comments: false } }) : null,
-];
-
 export default [
   {
     input: "./react-dom.js",
     output: { format: "esm", sourcemap: true, file: `./dist/react-dom.${env}.js` },
-    plugins: pluginsAlias,
+    plugins,
   },
   {
     input: "./react-dom.client.js",
     output: { format: "esm", sourcemap: true, file: `./dist/react-dom.client.${env}.js` },
-    plugins: pluginsAlias,
+    plugins,
   },
   {
     input: "./react-dom.server.browser.js",
     output: { format: "esm", sourcemap: true, file: `./dist/react-dom.server.browser.${env}.js` },
-    plugins: pluginsAlias,
+    plugins,
   },
   {
     input: "./react-dom.server.node.js",
     output: { format: "esm", sourcemap: true, file: `./dist/react-dom.server.node.${env}.js` },
-    plugins: pluginsAlias,
-  },
-  {
-    input: "./react-dom.js",
-    output: { format: "esm", sourcemap: true, file: `./dist/react-dom.${version}.${env}.js` },
-    plugins: pluginsSpecificVersion,
-  },
-  {
-    input: "./react-dom.client.js",
-    output: {
-      format: "esm",
-      sourcemap: true,
-      file: `./dist/react-dom.client.${version}.${env}.js`,
-    },
-    plugins: pluginsSpecificVersion,
-  },
-  {
-    input: "./react-dom.server.browser.js",
-    output: {
-      format: "esm",
-      sourcemap: true,
-      file: `./dist/react-dom.server.browser.${version}.${env}.js`,
-    },
-    plugins: pluginsSpecificVersion,
-  },
-  {
-    input: "./react-dom.server.node.js",
-    output: {
-      format: "esm",
-      sourcemap: true,
-      file: `./dist/react-dom.server.node.${version}.${env}.js`,
-    },
-    plugins: pluginsSpecificVersion,
+    plugins,
   },
 ];
