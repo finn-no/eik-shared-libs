39dfa96d34f2e2f7eb1ea8fe7765069eae49f626
Richard Walker <digitalsadhu@gmail.com>
2024-08-20 15:05:58 +1200

    fix: bypass the node resolve algorithm that was using the node.js version

--- packages/lit-labs-ssr-client/esbuild.js
+++ packages/lit-labs-ssr-client/esbuild.js
@@ -1,15 +1,19 @@
+import { join, dirname } from "node:path";
 import { createRequire } from "module";
 import { build } from "../../esbuild.js";
 
 const { resolve } = createRequire(import.meta.url);
 
+const renderLightDirectivePath = resolve("@lit-labs/ssr-client/directives/render-light.js");
+const litElementHydrateSupportPath = join(dirname(renderLightDirectivePath), '..', 'lit-element-hydrate-support.js');
+
 await Promise.all([
   build({
-    entryPoints: [resolve("@lit-labs/ssr-client/lit-element-hydrate-support.js")],
+    entryPoints: [litElementHydrateSupportPath],
     outfile: "./dist/lit-element-hydrate-support.js",
   }),
   build({
-    entryPoints: [resolve("@lit-labs/ssr-client/directives/render-light.js")],
+    entryPoints: [renderLightDirectivePath],
     outfile: "./dist/directives/render-light.js",
   }),
 ]);
