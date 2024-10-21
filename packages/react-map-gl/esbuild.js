842fb9d95480267793c7656693e2ad8b67cac463
William Killerud <william.killerud@schibsted.com>
2024-10-21 14:02:30 +0200

    Set up eik import mapping of React

--- packages/react-map-gl/esbuild.js
+++ packages/react-map-gl/esbuild.js
@@ -1,10 +1,14 @@
-import { build } from "../../esbuild.js";
 import { createRequire } from "module";
+import * as eik from "@eik/esbuild-plugin";
+import { build } from "../../esbuild.js";
 
 const { resolve } = createRequire(import.meta.url);
+
+await eik.load();
 
 await build({
   entryPoints: [resolve("react-map-gl")],
   outfile: "./dist/index.js",
-  external: ["mapbox-gl", "react", "react-dom"]
+  external: ["mapbox-gl"],
+  plugins: [eik.plugin()],
 });
