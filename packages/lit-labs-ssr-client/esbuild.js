2b679ff53281fa8f2870e9c223da12c6d7e497ca
Richard Walker <digitalsadhu@gmail.com>
2025-09-04 16:52:17 +0200

    fix: use same lit version as on prod

--- packages/lit-labs-ssr-client/esbuild.js
+++ packages/lit-labs-ssr-client/esbuild.js
@@ -1,5 +1,6 @@
 import { join, dirname } from "node:path";
 import { createRequire } from "module";
+import * as eik from "@eik/esbuild-plugin";
 import { build } from "../../esbuild.js";
 
 const { resolve } = createRequire(import.meta.url);
@@ -7,10 +8,17 @@ const { resolve } = createRequire(import.meta.url);
 const renderLightDirectivePath = resolve("@lit-labs/ssr-client/directives/render-light.js");
 const litElementHydrateSupportPath = join(dirname(renderLightDirectivePath), '..', 'lit-element-hydrate-support.js');
 
+await eik.load({
+  maps: [
+    { imports: { "lit-html": "https://assets.finn.no/npm/lit/v3/lit.min.js" } }
+  ],
+});
+
 await Promise.all([
   build({
     entryPoints: [litElementHydrateSupportPath],
     outfile: "./dist/lit-element-hydrate-support.js",
+    plugins: [eik.plugin()],
   }),
   build({
     entryPoints: [renderLightDirectivePath],
