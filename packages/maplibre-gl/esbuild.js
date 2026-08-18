62eb78ca265693770b2c9fee7cd1ff3876ea2f8d
Richard Walker <richard.walker@m10s.io>
2026-08-18 08:35:12 +0200

    chore(maplibre-gl): update resolve mechanism for 6.4.0

--- packages/maplibre-gl/esbuild.js
+++ packages/maplibre-gl/esbuild.js
@@ -2,8 +2,8 @@ import { build } from "../../esbuild.js";
 import { createRequire } from "module";
 
 const { resolve } = createRequire(import.meta.url);
-
+console.log(new URL(import.meta.resolve("maplibre-gl")).pathname)
 await build({
-  entryPoints: [resolve("maplibre-gl")],
+  entryPoints: [new URL(import.meta.resolve("maplibre-gl")).pathname],
   outfile: "./dist/index.js",
 });
